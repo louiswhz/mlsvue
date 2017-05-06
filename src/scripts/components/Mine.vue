1948a739cc606afd58d2cc931db97ec0f277be3a
haohongxing1 <369391558@qq.com>
2017-05-06 22:04:40 +0800

    denglu

---
+++ src/scripts/components/Mine.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="m-mine">
+		<header class="yo-header">
+			<h2 class="title">登录</h2>
+			<span class="regret yo-ico">&#xf07d;</span>
+			<span class="affirm yo-ico">&#xf07e;</span>
+		</header>
+		<section>
+			<h3 class="zhanghao">美丽说账号</h3>
+			<input type="text"  placeholder="昵称/邮箱/手机" class="shouji"/>
+			<h3 class="zhanghao">密码</h3>
+			<input type="text"  placeholder="输入密码" class="shouji"/>
+			<h3 class="zhanghao">翻转至正确方向
+				<span>换一组</span>
+			</h3>
+			<ul class="pic">
+				<li class="pic1"><img src="../../../images/1.jpg"/></li>
+				<li><img src="../../../images/2.jpg"></li>
+				<li><img src="../../../images/3.jpg"></li>
+				<li class="pic2"><img src="../../../images/4.jpg"></li>
+			</ul>
+			<input type="button" class="denglu" value="登录"/>
+			<div class="zhuce">
+				<span class="haiwai">海外手机</span>
+				<span class="mianmi">免密登录</span>
+				<span class="zhucezh">注册账号</span>
+			</div>
+			<div class="xian">
+			<sapn class="left"></sapn>
+			其他登录方式
+			<sapn class="right"></sapn>
+			</div>
+			<ul class="pic111">
+				<li class="pic11"><img src="../../../images/images/4.jpg"/></li>
+				<li><img src="../../../images/images/5.jpg"></li>
+				
+				<li class="pic22"><img src="../../../images/images/6.jpg"></li>
+			</ul>
+		</section>
+	</div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
