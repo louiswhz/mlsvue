10c0a6af690e3a9e93cc11cb15b07697cc314b80
uuuuRe <987864285@qq.com>
2017-05-06 12:12:37 +0800

    06-12:10-swipe-2

--- src/scripts/components/Board.vue
+++ src/scripts/components/Board.vue
@@ -13,10 +13,14 @@
         <mt-swipe-item><img src="https://s2.mogucdn.com/mlcdn/c45406/170503_735ej1j3a76ehffd6a1l5l09j25h9_750x500.jpg_750x999.v1c7E.70.webp"></mt-swipe-item :key="index">
         <mt-swipe-item><img src="https://s2.mogucdn.com/mlcdn/c45406/170504_4k54c6k053l76le0lil7chdbb9ah3_750x500.jpg_750x999.v1c7E.70.webp"></mt-swipe-item :key="index">
       </mt-swipe>
+
+      <div>
+        
+      </div>
     </section>
     <div class="m-tabbar">
         <div>
-          <router-link to="/board"><i class="yo-ico">&#xe729;</i><b>首页</b></router-link>
+          <router-link to="/board" active-class="active"><i class="yo-ico">&#xe729;</i><b>首页</b></router-link>
           <router-link to="/classify"><i class="yo-ico">&#xe612;</i><b>分类</b></router-link>
           <router-link to="/minicar"><i class="yo-ico">&#xe678;</i><b>购物车</b></router-link>
           <router-link to="/mine"><i class="yo-ico">&#xe629;</i><b>我</b></router-link>
