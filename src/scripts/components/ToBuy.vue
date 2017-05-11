7fa6736f04dcc680a06f4d31898057ac6d29f87d
wangdashenzaixian <2249169092@qq.com>
2017-05-11 18:24:06 +0800

    05.11_18:23_gao

--- src/scripts/components/ToBuy.vue
+++ src/scripts/components/ToBuy.vue
@@ -2,7 +2,7 @@
 	<div class="toy"  v-if="info">
 		<header class="header">
 			<span class="yo-ico" @click="back">&#xf07d;</span>
-		    <span><i class="yo-ico">&#xf07e;</i><i class="yo-ico right">&#xe602;</i></span>		    
+		    <span><i class="yo-ico">&#xf07e;</i><i class="yo-ico right">&#xe602;</i><b>0</b></span>		    
 	   </header>
 	   
        <section>
@@ -24,7 +24,26 @@
 		   	  	<span v-text="info.columns[1]"></span>
 		   	  	<span v-text="info.columns[2]"></span>
 		   	  </div>
-	   	  </div>	   	  
+	   	  </div>
+	   	  <ul class="buy_th">
+	   	  	<li><i class="yo-ico">&#xe616;</i><span>退货补运费</span></li>
+	   	  	<li><i class="yo-ico">&#xe616;</i><span>7天无理由退货</span></li>
+	   	  	<li><i class="yo-ico">&#xe616;</i><span>先行赔付</span></li>
+	   	  </ul>
+	   	  
+	   	  <div class="car_privilege">
+	   	  	<h3>优惠活动</h3>
+	   	  	<ul>
+	   	  		<li><p><span>￥</span><b>3</b></p><i>满99减</i></li>
+	   	  		<li><p><span>￥</span><b>5</b></p><i>满129减</i></li>
+	   	  		<li><p><span>￥</span><b>10</b></p><i>满199减</i></li>
+	   	  	</ul>
+	   	  </div>
+	   	  <div class="car_mlj">
+	   	  	<p><b>满件立减</b></p>
+	   	  	<span>满两件立减5元</span>
+	   	  </div>
+	   	  
 	   </section>
 	   <div class="car_shop">
 	   	  	<ul>
@@ -56,6 +75,29 @@
 	   	  		</div>
 	   	  		<span @click="hidemodal">X</span>
 	   	  	</div>
+	   	  	<div class="car_wpxz">
+	   	  		<div class="clothes_color">
+	   	  			<h3>颜色：</h3>
+	   	  			<ul class="color_xz" >
+	   	  				<li v-for="item in title">{{item.style}}</li>	
+	   	  			</ul>
+	   	  		</div>
+	   	  		<div class="clothes_size">
+	   	  			<h3>尺码：</h3>
+	   	  			<ul class="size_xz">
+	   	  				<li v-for="item in title">{{item.size}}</li>	   	  				
+	   	  			</ul>
+	   	  		</div>
+	   	  		<div class="clothes_num">
+	   	  			<h3>数量：</h3>
+	   	  			<div class="num_xz">
+	   	  				<span>-</span>
+	   	  			    <span class="num">0</span>
+	   	  			    <span>+</span>
+	   	  			</div>	   	  			
+	   	  		</div>
+	   	  		<div class="clothes_confirm"><p></p><span>确定</span></div>
+	   	  	</div>
 		  </mt-popup>
    </div>
 </template>
@@ -70,7 +112,8 @@
 		data() {
 	      return {
 	        info: null,
-	        popupVisible:false
+	        popupVisible:false,
+	        title:[]
 	      }
 	    },
 	    
@@ -98,7 +141,14 @@
         method: 'get',
         callback: function (res) {
             that.info = res.data.result
-          
+            that.title= res.data.result.skuInfo.skus
+            that.name=res.data.result.skuInfo.skus
+            
+            console.log(res.data.result.skuInfo.skus.stock)
+            
+//          for(var i = 0; i < that.title.length ; i++){
+//          	console.log(that.title[i].size)
+//          }
         }
       })
     }
